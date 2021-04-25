38d14fd2a7403863fb2e075f8c39d02c7b0968d9
august1n3 <79272623+august1n3@users.noreply.github.com>
2021-04-26 00:44:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-const TaxPayer = require('./taxpayer')
+const TaxPayer = require('./models/taxpayer')
 
 require('dotenv/config')
 
